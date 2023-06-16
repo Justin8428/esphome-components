b1a764c540006295d48e39575e377d0d5cd57b23
jymbob <james.scholes@gmail.com>
2023-06-16 11:22:21 +0100

    Update lampsmart_pro_light.cpp - try again

--- components/lampsmart_pro_light/lampsmart_pro_light.cpp
+++ components/lampsmart_pro_light/lampsmart_pro_light.cpp
@@ -156,7 +156,7 @@ void LampSmartProLight::send_packet(uint16_t cmd, uint8_t cold, uint8_t warm) {
   uint16_t seed = (uint16_t) rand();
 
   adv_data_t packet = {{
-      .prefix = {0x02, 0x01, 0x01, 0x1B, 0x03, 0xF0, 0x08, 0x10, 0x80, 0x00},
+      .prefix = {0x02, 0x01, 0x01, 0x1B, 0x03, 0x77, 0xF8, 0xB6, 0x5F, 0x00},
       .packet_number = ++(this->tx_count_),
       .type = 0x100,
       .identifier = light_state_ ? light_state_->get_object_id_hash() : 0xcafebabe,
